1c1418b0d5de24db2882c6ee465fc3e76a19adbb
Aryan Jaiswal <162091399+AryanJaiswal763@users.noreply.github.com>
2025-08-19 19:29:12 +0530

    Time: 1 ms (33.88%), Space: 45.4 MB (42.79%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,25 @@
 class Solution {
     public int missingNumber(int[] nums) {
+     
+      // optimal with O(n);
+      int sum=0;
+      for(Integer num:nums)
+      sum=sum+num;
+      
+      int n=nums.length;
+      int total=n*(n+1)/2;
 
-        Arrays.sort(nums);
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]!=i)
-            return i;
-        }
+      return total-sum;
 
-        return nums.length;
+
+     // By sorting- Complexity-O(nlogn)
+        // Arrays.sort(nums);
+        // for(int i=0; i<nums.length; i++){
+        //     if(nums[i]!=i)
+        //     return i;
+        // }
+
+        // return nums.length;
     }
 }
 // 0 1 3
