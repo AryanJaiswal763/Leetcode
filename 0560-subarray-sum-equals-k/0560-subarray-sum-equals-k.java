cf179a1cce46459ef55effaa3df360462240fef7
Aryan Jaiswal <162091399+AryanJaiswal763@users.noreply.github.com>
2025-08-29 16:44:36 +0530

    Time: 17 ms (99.85%), Space: 47.6 MB (5.98%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,17 +1,44 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-      
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum=sum+nums[j];
-                if(sum==k){
-                count++;
-                }
+     
+     
+     int count=0;
+     Map<Integer,Integer>map=new HashMap<>();
 
-            }
+     int[] prefixSum=new int[nums.length];
+     prefixSum[0]=nums[0];
+
+     for(int i=1;i<nums.length;i++){
+        prefixSum[i]=prefixSum[i-1]+nums[i];
+     }
+
+     for(int i=0;i<nums.length;i++){
+        if(prefixSum[i]==k)
+        count++;
+
+        int val=prefixSum[i]-k;
+        if(map.containsKey(val)){
+            count=count+map.get(val);
         }
+
+        map.put(prefixSum[i], map.getOrDefault(prefixSum[i],0)+1);
+        
+     }
+
+
+
+
+
+        // for(int i=0;i<nums.length;i++){
+        //     int sum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //         sum=sum+nums[j];
+        //         if(sum==k){
+        //         count++;
+        //         }
+
+        //     }
+        // }
 
         return count;
     }
