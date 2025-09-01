9c4736d1a720aa095e5e5c8e31547902706d28f1
Aryan Jaiswal <162091399+AryanJaiswal763@users.noreply.github.com>
2025-09-01 17:54:37 +0530

    Time: 1 ms (99.85%), Space: 48.9 MB (36.18%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,22 +10,37 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode head, ListNode head1) {
-        
-        HashSet<ListNode>set=new HashSet<>();
-        while(head!=null){
-            set.add(head);
-            head=head.next;
+    public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
+         
+        // O(1) space complexity
+        ListNode temp1=head1;
+        ListNode temp2=head2;
+
+        while(temp1!=temp2){
+            temp1=(temp1==null)?head2:temp1.next;
+            temp2=(temp2==null)?head1:temp2.next;
         }
 
-        while(head1!=null){
-            if(set.contains(head1))
-            return head1;
+        return temp1;
 
-            set.add(head1);
-            head1=head1.next;
-        }
 
-        return null;
+
+        //O(n) space solution
+
+        // HashSet<ListNode>set=new HashSet<>();
+        // while(head!=null){
+        //     set.add(head);
+        //     head=head.next;
+        // }
+
+        // while(head1!=null){
+        //     if(set.contains(head1))
+        //     return head1;
+
+        //     set.add(head1);
+        //     head1=head1.next;
+        // }
+
+        // return null;
     }
 }
