02753bc45bb96b3201c297109a7d1ec4026997fe
Aryan Jaiswal <162091399+AryanJaiswal763@users.noreply.github.com>
2025-09-01 17:21:45 +0530

    Time: 1 ms (100%), Space: 44.7 MB (29.32%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -11,39 +11,46 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode head1, ListNode head2) {
         
-        ListNode result=new ListNode(0);
-        ListNode ptr=result;
+
+        int num=head1.val+head2.val;
+        ListNode head3=new ListNode(num%10);
+        ListNode temp=head3;
+        int carry=num/10;
         
-        int carry=0;
+        head1=head1.next;
+        head2=head2.next;
 
-        while(head1!=null || head2!=null)
-        {
-            int sum=carry;
-            if(head1!=null)
-            {
-                sum=sum+head1.val;
-                head1=head1.next;
-              
-            }
-            if(head2!=null)
-            {
-                sum=sum+head2.val;
-                head2=head2.next;
-            }
-
-            carry=sum/10;
-            sum=sum%10;
-            ptr.next=new ListNode(sum);
-            ptr=ptr.next;
-
-        }
-        if(carry==1)
-        {
-            ptr.next=new ListNode(1);
-            ptr=ptr.next;
+        while(head1!=null && head2!=null){
+            int nums=head1.val+head2.val;
+            ListNode temp2=new ListNode( (nums+carry)%10 );
+            carry=(nums+carry)/10;
+            temp.next=temp2;
+            temp=temp2;
+            head1=head1.next;
+            head2=head2.next;
         }
 
-        return result.next;
-       
+        while(head1!=null){
+             ListNode temp3=new ListNode( (head1.val+carry)%10 );
+             carry=(head1.val+carry)/10;
+             temp.next=temp3;
+             temp=temp3;
+             head1=head1.next;
+        }
+
+        while(head2!=null){
+            ListNode temp4=new ListNode( (head2.val+carry)%10 );
+             carry=(head2.val+carry)/10;
+             temp.next=temp4;
+             temp=temp4;
+             head2=head2.next;
+        }
+
+        if(carry==1){
+               ListNode temp5=new ListNode(1);
+               temp.next=temp5;
+        }
+
+        return head3;
     }
 }
