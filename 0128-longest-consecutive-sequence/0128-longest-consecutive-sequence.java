9fe33b140ec214823ea9f3fa08f6db806a58acf7
Aryan Jaiswal <162091399+AryanJaiswal763@users.noreply.github.com>
2025-09-03 22:30:55 +0530

    Time: 43 ms (12.16%), Space: 59.2 MB (72.35%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,30 +1,59 @@
 class Solution {
+    public static int findMaxLength(HashMap<Integer,Boolean>map, int key){
+        int ans=0;
+        while(map.containsKey(key)){
+            ans++;
+            key++;
+        }
+
+        return ans;
+    }
     public int longestConsecutive(int[] nums) {
         
 // 1 2 3 4 100 200
 // 0 1 1 2
-        // O{nlongn} time complexity
 
-        Arrays.sort(nums);
-        int  maxlen=1, len=1;
-        for(int i=1;i<nums.length;i++){
-            
-            if(nums[i]==nums[i-1])
-            continue;
+        HashMap<Integer,Boolean>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],true);
+        }
+ 
 
-            else if(nums[i-1]+1==nums[i]){
-               len++;
-               maxlen=Math.max(maxlen,len);
-            }
-            else{
-                len=1;
+        for(Integer num: nums){
+            if(map.containsKey(num-1))
+            map.put(num,false);
+        }
+        int maxLen=0;
+        for(Integer key:map.keySet()){
+            if(map.get(key)==true){
+               maxLen=Math.max(maxLen, findMaxLength(map,key));
             }
         }
 
-         if(nums.length==0)
-         return 0;
-         
-        return maxlen;
+        return maxLen;
+
+        // O{nlongn} time complexity
+
+        // Arrays.sort(nums);
+        // int  maxlen=1, len=1;
+        // for(int i=1;i<nums.length;i++){
+            
+        //     if(nums[i]==nums[i-1])
+        //     continue;
+
+        //     else if(nums[i-1]+1==nums[i]){
+        //        len++;
+        //        maxlen=Math.max(maxlen,len);
+        //     }
+        //     else{
+        //         len=1;
+        //     }
+        // }
+
+        //  if(nums.length==0)
+        //  return 0;
+
+        // return maxlen;
 
     }
 }
