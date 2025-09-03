6cf26e1ac8a66ae07ffda5fdbe06267af5e9089a
Aryan Jaiswal <162091399+AryanJaiswal763@users.noreply.github.com>
2025-09-03 20:55:04 +0530

    Time: 0 ms (100%), Space: 42.3 MB (61.11%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,32 +1,58 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        ArrayList<Integer>list=new ArrayList<>();
-        int i=0,j=0;
-        while(i<m && j<n){
-          if(nums1[i]<=nums2[j]){
-          list.add(nums1[i]);
-          i++;
-          }
+        int ind=m+n-1;
+        int i=m-1, j=n-1;
 
-          else{
-            list.add(nums2[j]);
-            j++;
-          }
+        while(i>=0 && j>=0){
+            if(nums1[i]>=nums2[j]){
+                nums1[ind]=nums1[i];
+                i--;
+            }
+            else{
+                nums1[ind]=nums2[j];
+                j--;
+            }
+
+            ind--;
         }
 
-        while(i<m){
-            list.add(nums1[i]);
-            i++;
-        }
+         while(j>=0){
+            nums1[ind]=nums2[j];
+            j--;
+            ind--;
+         }
 
-        while(j<n){
-            list.add(nums2[j]);
-            j++;
-        }
 
-        for(int k=0;k<list.size();k++){
-            nums1[k]=list.get(k);
-        }
+
+        // Using extra space O(n);
+
+        // ArrayList<Integer>list=new ArrayList<>();
+        // int i=0,j=0;
+        // while(i<m && j<n){
+        //   if(nums1[i]<=nums2[j]){
+        //   list.add(nums1[i]);
+        //   i++;
+        //   }
+
+        //   else{
+        //     list.add(nums2[j]);
+        //     j++;
+        //   }
+        // }
+
+        // while(i<m){
+        //     list.add(nums1[i]);
+        //     i++;
+        // }
+
+        // while(j<n){
+        //     list.add(nums2[j]);
+        //     j++;
+        // }
+
+        // for(int k=0;k<list.size();k++){
+        //     nums1[k]=list.get(k);
+        // }
     }
 }
